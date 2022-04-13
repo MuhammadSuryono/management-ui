bede8c537e359a89bc7cbea8708d90562a134d3d
Muhammad Suryono <msuryono@gmail.com>
2022-04-13 09:54:32 +0700

    Initial commit

---
+++ application/logs/log-2022-04-13.php
@@ -0,0 +1,132 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2022-04-13 02:48:22 --> Config Class Initialized
+INFO - 2022-04-13 02:48:22 --> Hooks Class Initialized
+DEBUG - 2022-04-13 02:48:22 --> UTF-8 Support Enabled
+INFO - 2022-04-13 02:48:22 --> Utf8 Class Initialized
+INFO - 2022-04-13 02:48:22 --> URI Class Initialized
+INFO - 2022-04-13 02:48:22 --> Router Class Initialized
+INFO - 2022-04-13 02:48:22 --> Output Class Initialized
+INFO - 2022-04-13 02:48:22 --> Security Class Initialized
+DEBUG - 2022-04-13 02:48:22 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-04-13 02:48:22 --> Input Class Initialized
+INFO - 2022-04-13 02:48:22 --> Language Class Initialized
+INFO - 2022-04-13 02:48:22 --> Loader Class Initialized
+INFO - 2022-04-13 02:48:22 --> Helper loaded: url_helper
+INFO - 2022-04-13 02:48:22 --> Helper loaded: form_helper
+INFO - 2022-04-13 02:48:22 --> Helper loaded: string_helper
+INFO - 2022-04-13 02:48:22 --> Helper loaded: file_helper
+INFO - 2022-04-13 02:48:22 --> Helper loaded: number_helper
+INFO - 2022-04-13 02:48:22 --> Session: Class initialized using 'files' driver.
+DEBUG - 2022-04-13 02:48:22 --> Session class already loaded. Second attempt ignored.
+INFO - 2022-04-13 02:48:22 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2022-04-13 02:48:22 --> Pagination Class Initialized
+INFO - 2022-04-13 02:48:22 --> Form Validation Class Initialized
+INFO - 2022-04-13 02:48:22 --> User Agent Class Initialized
+INFO - 2022-04-13 02:48:22 --> Controller Class Initialized
+INFO - 2022-04-13 02:48:22 --> Config Class Initialized
+INFO - 2022-04-13 02:48:22 --> Hooks Class Initialized
+DEBUG - 2022-04-13 02:48:22 --> UTF-8 Support Enabled
+INFO - 2022-04-13 02:48:22 --> Utf8 Class Initialized
+INFO - 2022-04-13 02:48:22 --> URI Class Initialized
+DEBUG - 2022-04-13 02:48:22 --> No URI present. Default controller set.
+INFO - 2022-04-13 02:48:22 --> Router Class Initialized
+INFO - 2022-04-13 02:48:22 --> Output Class Initialized
+INFO - 2022-04-13 02:48:22 --> Security Class Initialized
+DEBUG - 2022-04-13 02:48:22 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-04-13 02:48:22 --> Input Class Initialized
+INFO - 2022-04-13 02:48:22 --> Language Class Initialized
+INFO - 2022-04-13 02:48:22 --> Loader Class Initialized
+INFO - 2022-04-13 02:48:22 --> Helper loaded: url_helper
+INFO - 2022-04-13 02:48:22 --> Helper loaded: form_helper
+INFO - 2022-04-13 02:48:22 --> Helper loaded: string_helper
+INFO - 2022-04-13 02:48:22 --> Helper loaded: file_helper
+INFO - 2022-04-13 02:48:22 --> Helper loaded: number_helper
+INFO - 2022-04-13 02:48:22 --> Session: Class initialized using 'files' driver.
+DEBUG - 2022-04-13 02:48:22 --> Session class already loaded. Second attempt ignored.
+INFO - 2022-04-13 02:48:22 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2022-04-13 02:48:23 --> Pagination Class Initialized
+INFO - 2022-04-13 02:48:23 --> Form Validation Class Initialized
+INFO - 2022-04-13 02:48:23 --> User Agent Class Initialized
+INFO - 2022-04-13 02:48:23 --> Controller Class Initialized
+INFO - 2022-04-13 02:48:23 --> Config Class Initialized
+INFO - 2022-04-13 02:48:23 --> Hooks Class Initialized
+DEBUG - 2022-04-13 02:48:23 --> UTF-8 Support Enabled
+INFO - 2022-04-13 02:48:23 --> Utf8 Class Initialized
+INFO - 2022-04-13 02:48:23 --> URI Class Initialized
+DEBUG - 2022-04-13 02:48:23 --> No URI present. Default controller set.
+INFO - 2022-04-13 02:48:23 --> Router Class Initialized
+INFO - 2022-04-13 02:48:23 --> Output Class Initialized
+INFO - 2022-04-13 02:48:23 --> Security Class Initialized
+DEBUG - 2022-04-13 02:48:23 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-04-13 02:48:23 --> Input Class Initialized
+INFO - 2022-04-13 02:48:23 --> Language Class Initialized
+INFO - 2022-04-13 02:48:23 --> Loader Class Initialized
+INFO - 2022-04-13 02:48:23 --> Helper loaded: url_helper
+INFO - 2022-04-13 02:48:23 --> Helper loaded: form_helper
+INFO - 2022-04-13 02:48:23 --> Helper loaded: string_helper
+INFO - 2022-04-13 02:48:23 --> Helper loaded: file_helper
+INFO - 2022-04-13 02:48:23 --> Helper loaded: number_helper
+INFO - 2022-04-13 02:48:23 --> Session: Class initialized using 'files' driver.
+DEBUG - 2022-04-13 02:48:23 --> Session class already loaded. Second attempt ignored.
+INFO - 2022-04-13 02:48:23 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2022-04-13 02:48:23 --> Pagination Class Initialized
+INFO - 2022-04-13 02:48:23 --> Form Validation Class Initialized
+INFO - 2022-04-13 02:48:23 --> User Agent Class Initialized
+INFO - 2022-04-13 02:48:23 --> Controller Class Initialized
+INFO - 2022-04-13 02:48:23 --> Config Class Initialized
+INFO - 2022-04-13 02:48:23 --> Hooks Class Initialized
+DEBUG - 2022-04-13 02:48:23 --> UTF-8 Support Enabled
+INFO - 2022-04-13 02:48:23 --> Utf8 Class Initialized
+INFO - 2022-04-13 02:48:23 --> URI Class Initialized
+INFO - 2022-04-13 02:48:23 --> Router Class Initialized
+INFO - 2022-04-13 02:48:23 --> Output Class Initialized
+INFO - 2022-04-13 02:48:23 --> Security Class Initialized
+DEBUG - 2022-04-13 02:48:23 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-04-13 02:48:23 --> Input Class Initialized
+INFO - 2022-04-13 02:48:23 --> Language Class Initialized
+INFO - 2022-04-13 02:48:23 --> Loader Class Initialized
+INFO - 2022-04-13 02:48:23 --> Helper loaded: url_helper
+INFO - 2022-04-13 02:48:23 --> Helper loaded: form_helper
+INFO - 2022-04-13 02:48:23 --> Helper loaded: string_helper
+INFO - 2022-04-13 02:48:23 --> Helper loaded: file_helper
+INFO - 2022-04-13 02:48:23 --> Helper loaded: number_helper
+INFO - 2022-04-13 02:48:23 --> Session: Class initialized using 'files' driver.
+DEBUG - 2022-04-13 02:48:23 --> Session class already loaded. Second attempt ignored.
+INFO - 2022-04-13 02:48:23 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2022-04-13 02:48:23 --> Pagination Class Initialized
+INFO - 2022-04-13 02:48:23 --> Form Validation Class Initialized
+INFO - 2022-04-13 02:48:23 --> User Agent Class Initialized
+INFO - 2022-04-13 02:48:23 --> Controller Class Initialized
+INFO - 2022-04-13 02:48:23 --> File loaded: C:\yono\application\management-data-ui\application\views\auth/login.php
+INFO - 2022-04-13 02:48:23 --> File loaded: C:\yono\application\management-data-ui\application\views\auth/wrapper.php
+INFO - 2022-04-13 02:48:23 --> Final output sent to browser
+DEBUG - 2022-04-13 02:48:23 --> Total execution time: 0.0587
+INFO - 2022-04-13 02:48:30 --> Config Class Initialized
+INFO - 2022-04-13 02:48:30 --> Hooks Class Initialized
+DEBUG - 2022-04-13 02:48:30 --> UTF-8 Support Enabled
+INFO - 2022-04-13 02:48:30 --> Utf8 Class Initialized
+INFO - 2022-04-13 02:48:30 --> URI Class Initialized
+INFO - 2022-04-13 02:48:30 --> Router Class Initialized
+INFO - 2022-04-13 02:48:30 --> Output Class Initialized
+INFO - 2022-04-13 02:48:30 --> Security Class Initialized
+DEBUG - 2022-04-13 02:48:30 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-04-13 02:48:30 --> Input Class Initialized
+INFO - 2022-04-13 02:48:30 --> Language Class Initialized
+INFO - 2022-04-13 02:48:30 --> Loader Class Initialized
+INFO - 2022-04-13 02:48:30 --> Helper loaded: url_helper
+INFO - 2022-04-13 02:48:30 --> Helper loaded: form_helper
+INFO - 2022-04-13 02:48:30 --> Helper loaded: string_helper
+INFO - 2022-04-13 02:48:30 --> Helper loaded: file_helper
+INFO - 2022-04-13 02:48:30 --> Helper loaded: number_helper
+INFO - 2022-04-13 02:48:30 --> Session: Class initialized using 'files' driver.
+DEBUG - 2022-04-13 02:48:30 --> Session class already loaded. Second attempt ignored.
+INFO - 2022-04-13 02:48:30 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2022-04-13 02:48:30 --> Pagination Class Initialized
+INFO - 2022-04-13 02:48:30 --> Form Validation Class Initialized
+INFO - 2022-04-13 02:48:30 --> User Agent Class Initialized
+INFO - 2022-04-13 02:48:30 --> Controller Class Initialized
+INFO - 2022-04-13 02:48:30 --> File loaded: C:\yono\application\management-data-ui\application\views\auth/login.php
+INFO - 2022-04-13 02:48:30 --> File loaded: C:\yono\application\management-data-ui\application\views\auth/wrapper.php
+INFO - 2022-04-13 02:48:30 --> Final output sent to browser
+DEBUG - 2022-04-13 02:48:30 --> Total execution time: 0.0689
